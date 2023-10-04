a8a7b092c7b7d59d8aff563de44f0f363f3b9a52
gabrielgoncalo <gabriel.tchutcha@hotmail.com.br>
2023-10-04 06:11:05 -0300

    aula sla qual

---
+++ mycontacs/src/app/controllers/contactController.js
@@ -0,0 +1,27 @@
+class contactController {
+    index() {
+        // Lista todos os registros
+
+    }
+
+    show() {
+        // Obter um registro
+    }
+
+    store() {
+        // Criar novo registro
+
+    }
+
+    update() {
+        // Editar um registro
+
+    }
+
+    delete(){
+        // Deletar um registro
+
+    }
+}
+//metodo Singleton
+module.exports = new contactController();
--- mycontacs/src/index.js
+++ mycontacs/src/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
-
-app.get('/', (request, response) => {
-    response.send('Hello world!');
-
-});
+app.use(routes);
 
 app.listen(3000, () => console.log('server start at http://localhost:3000'));
